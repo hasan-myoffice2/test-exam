a7db0f8c7a806060f2c5e14dfc05c2d11bcd7720
Hasan <hasan.myoffice2@gmail.com>
2026-04-20 09:19:59 +0600

    Project Complate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,315 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- Header Section -->
+  <nav>
+    <div class="nav-container">
+      <div class="brand-name">
+        <img src="/public/images/brand-name.png" alt="">
+      </div>
+      <ul class="nav-menu">
+        <li class="active">Home</li>
+        <li>About Us</li>
+        <li>Team</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+        <li>Contact Us</li>
+      </ul>
+      <div class="icons"><i class="fa-solid fa-bars"></i></div>
+    </div>
+  </nav>
+  <!-- Header Section End-->
+
+  <!-- Hero Section -->
+  <section>
+    <div class="hero-section">
+      <div class="title-aria">
+        <span class="agency">- Consulting Agency</span>
+        <h2>Businees & Technology Consulting</h2>
+        <p>Digital, business & technology consulting for business resilience.</p>
+        <div class="btn-group">
+          <button class="btn-1">Get registered</button>
+          <button class="btn-2">Learn more</button>
+        </div>
+      </div>
+      <div class="image">
+        <img src="/public/images/hero.jpg" alt="images">
+      </div>
+    </div>
+  </section>
+  <!-- Hero End -->
+
+  <!-- Service Section -->
+  <section class="service-section">
+    <div class="service-container">
+      <div class="service-title">
+        <h2>Service</h2>
+        <p>Various versions have evolved over the years, sometimes by accident <br> excepteur sint occaecat cupidatat
+          non
+          proident over the years.</p>
+      </div>
+      <div class="service-card">
+        <div class="service-items">
+          <img src="/public/images/image2.png" alt="image">
+          <div class="service-content">
+            <h3>Digital Solution</h3>
+            <p>On the other hand, we denounce with righteous indignation and dislike human nature.</p>
+            <a href="#" type="submit">More info</a>
+          </div>
+        </div>
+        <div class="service-items">
+          <img src="/public/images/image2.png" alt="image">
+          <div class="service-content">
+            <h3>Digital Solution</h3>
+            <p>On the other hand, we denounce with righteous indignation and dislike human nature.</p>
+            <a href="#" type="submit">More info</a>
+          </div>
+        </div>
+        <div class="service-items">
+          <img src="/public/images/image2.png" alt="image">
+          <div class="service-content">
+            <h3>Digital Solution</h3>
+            <p>On the other hand, we denounce with righteous indignation and dislike human nature.</p>
+            <a href="#" type="submit">More info</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Service End -->
+
+  <!-- About Section -->
+  <section class="about-section">
+    <div class="about-container">
+      <div class="about-aria">
+        <img src="/public/images/features.jpg" alt="image">
+        <div class="about-title">
+          <span class="expart">- Our expert</span>
+          <h2><span class="jack-merry">Jack Murry</span> - CEO and
+            co-founder of this
+            consulting</h2>
+          <p>Various versions have evolved over the years, sometimes by accident
+            excepteur sint occaecat cupidatat non proident over the years.</p>
+          <ul>
+            <li>✅ Product engineering</li>
+            <li>✅ Product engineering</li>
+            <li>✅ Product engineering</li>
+            <li>✅ Product engineering</li>
+            <li>✅ Product engineering</li>
+          </ul>
+          <button class="about-btn">About</button>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- About End -->
+
+  <!-- Testomial Section -->
+  <section class="testtimonial-section">
+    <div class="testimonial-container">
+      <div class="testomial-title">
+        <p class="span">- Our clients -</p>
+        <h2>What patients say</h2>
+        <p>Various versions have evolved over the years, sometimes by accident <br>
+          excepteur sint occaecat cupidatat non proident.</p>
+      </div>
+      <div class="testimonial-card">
+        <div class="card-item">
+          <div class="avator">
+            <img src="/public/images/testimonia.png" alt="testimonial">
+            <div class="avator-title">
+              <h3>Amber</h3>
+              <h4>students</h4>
+            </div>
+          </div>
+          <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. br Quisquam dolores veniam ea incidunt
+            dolor?</P>
+          <ul class="rating">
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+          </ul>
+        </div>
+        <div class="card-item">
+          <div class="avator">
+            <img src="/public/images/testimonia.png" alt="testimonia">
+            <div class="avator-title">
+              <h3>Amber</h3>
+              <h4>students</h4>
+            </div>
+          </div>
+          <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. br Quisquam dolores veniam ea incidunt
+            dolor?</P>
+          <ul class="rating">
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+          </ul>
+        </div>
+        <div class="card-item">
+          <div class="avator">
+            <img src="/public/images/testimonia.png" alt="testimonia">
+            <div class="avator-title">
+              <h3>Amber</h3>
+              <h4>students</h4>
+            </div>
+          </div>
+          <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. br Quisquam dolores veniam ea incidunt
+            dolor?</P>
+          <ul class="rating">
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+            <li>⭐</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Testomial End -->
+
+  <!-- Pricing-card Section -->
+  <div class="pricing-card">
+    <div class="pricing-container">
+      <div class="pricing-title">
+        <h2>Pricing Plans</h2>
+        <p>Various versions have evolved over the years, sometimes by accident <br>
+          excepteur sint occaecat cupidatat non proident.</p>
+      </div>
+      <div class="card">
+        <div class="card-items">
+          <h3>General session</h3>
+          <h2>$100</h2>
+          <hr>
+          <ul class="list-items">
+            <li>✅ 6 session/month</li>
+            <li>✅ Duration 30 minutes</li>
+            <li>✅ Consulting & Advisory</li>
+            <li>✅ Relationship Buildup</li>
+            <li>✅ Sales & Marketing</li>
+            <button>Get plan</button>
+          </ul>
+        </div>
+        <div class="card-items">
+          <h3>General session</h3>
+          <h2>$100</h2>
+          <hr>
+          <ul class="list-items">
+            <li>✅ 6 session/month</li>
+            <li>✅ Duration 30 minutes</li>
+            <li>✅ Consulting & Advisory</li>
+            <li>✅ Relationship Buildup</li>
+            <li>✅ Sales & Marketing</li>
+            <button>Get plan</button>
+          </ul>
+        </div>
+        <div class="card-items">
+          <h3>General session</h3>
+          <h2>$100</h2>
+          <hr>
+          <ul class="list-items">
+            <li>✅ 6 session/month</li>
+            <li>✅ Duration 30 minutes</li>
+            <li>✅ Consulting & Advisory</li>
+            <li>✅ Relationship Buildup</li>
+            <li>✅ Sales & Marketing</li>
+            <button>Get plan</button>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Pricing-card End -->
+
+  <!-- Work Section -->
+  <section class="article-section">
+    <div class="container">
+      <div class="article-title">
+        <h4>- blog & news -</h4>
+        <h2>Letest news articles</h2>
+        <p>Various versions have evolved over the years, sometimes by accident <br>
+          excepteur sint occaecat cupidatat non proident.</p>
+      </div>
+      <div class="article-card">
+        <div class="card-items">
+          <img src="/public/images/blog1.jpg" alt="image">
+          <div class="card-content">
+            <p class="date">06 June 2022 | 105 views | No Comments</p>
+            <h3>Sales and marketing design and improvement stratagy</h3>
+            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur adipisci velit,
+              sed
+              quia.</p>
+            <button>Reed More</button>
+          </div>
+        </div>
+        <div class="card-items">
+          <img src="/public/images/blog2.jpg" alt="image">
+          <div class="card-content">
+            <p class="date">06 June 2022 | 105 views | No Comments</p>
+            <h3>Sales and marketing design and improvement stratagy</h3>
+            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur adipisci velit,
+              sed
+              quia.</p>
+            <button>Reed More</button>
+          </div>
+        </div>
+        <div class="card-items">
+          <img src="/public/images/blog3.jpg" alt="image">
+          <div class="card-content">
+            <p class="date">06 June 2022 | 105 views | No Comments</p>
+            <h3>Sales and marketing design and improvement stratagy</h3>
+            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur adipisci velit,
+              sed
+              quia.</p>
+            <button>Reed More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Work End -->
+
+  <!-- Contact Section -->
+  <section class="contact">
+    <div class="contact-container">
+
+      <!-- LEFT -->
+      <div class="contact-left">
+        <h2>Contact us</h2>
+        <p>
+          Various versions have evolved over the years, sometimes by
+          excepteur sint occaecat cupidatat non proident.
+        </p>
+
+        <form>
+          <div class="input-row">
+            <input type="text" placeholder="Name :" required>
+            <input type="email" placeholder="Email :" required>
+          </div>
+
+          <input type="text" placeholder="Subject :" required>
+
+          <textarea rows="5" placeholder="Message :"></textarea>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <!-- RIGHT (MAP) -->
+      <div class="contact-right">
+        <iframe src="https://www.google.com/maps?q=Toronto&output=embed" loading="lazy">
+        </iframe>
+      </div>
+
+    </div>
+  </section>
+  <!-- Contact End -->
+
+</template>
